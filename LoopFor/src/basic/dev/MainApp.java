f4c4ecdd5d2199cc6569610c14a5cb47b066adc4
quangk22 <113655587+quangk22@users.noreply.github.com>
2022-11-03 11:01:59 +0700

    Add files via upload

---
+++ LoopFor/src/basic/dev/MainApp.java
@@ -0,0 +1,27 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1, num2, reversed = 0;
+		System.out.println("nhập n : ");
+		num1 = sc.nextInt();
+		num2 = num1;
+		for (; num1 != 0;num1 /= 10) {
+			 int digit = num1 % 10;
+		      reversed = reversed * 10 + digit;
+		}
+		System.out.printf("Số %d sau khi đảo ngược là: %d", num2, reversed);
+		if(reversed == num2) {
+			System.out.println(" là số polinom");
+
+		}else {
+			System.out.println("không phải số polinom");
+			}
+
+	}
+
+}
